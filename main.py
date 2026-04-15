b0336ab386a6bbb9b9408845e9f01349fefb7368
Thais <thaismedina@mail.weber.edu>
2026-04-15 15:03:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from servicio import ServicioTecnico
-
-def main():
-    app = ServicioTecnico()
-    app.iniciar()
-
-if __name__ == "__main__":
-    main()
+def menu(self):
+    print("\n=== Servicio Técnico Pro ===")
+    print(f"[Servicio Técnico]: Bienvenido {self.cliente}")
+    print(f"[Servicio Técnico]: {self.cliente}, seleccione una opción")
+    print("1️⃣ Revisar")
+    print("2️⃣ Reparar")
+    print("3️⃣ Donar")
+    return input(f"[{self.cliente}]: ")
